d710686d368073afbfa4f912cbd89adecd2f864d
ahoy-glitch <cesarjulio96746@gmail.com>
2023-01-14 21:53:43 -0300

    main

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from os import system
 from keyboard import is_pressed
 
 def theMainGuy():
+    #clear
     system("cls")
     print("Hi, i'll tell you if a number is zero or not.")
-    k = input("Press enter to continue.")
+    print("Press enter to continue.")
     
     while True:
         try:
@@ -12,24 +13,26 @@ def theMainGuy():
                 system("cls")
                 a = int(input("Enter a number: "))
                 theGuyWhoChecksNumber(a)
+                #breaking the loop
                 break
-        except:
+        except: 
                 system("cls")
                 a = int(input("Enter a number: "))
                 theGuyWhoChecksNumber(a)
+                #same
                 break
     
     
 
 
 def theGuyWhoChecksNumber(n):
-    
     if n == 0:
         #another clear
         system("cls")
         print("It's zero")
     elif n != 0:
+        #another clear
         system("cls")
         print("It's not zero")
-
+#calling the main guy function
 theMainGuy()
